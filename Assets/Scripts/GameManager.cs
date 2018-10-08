90ba4a1ea9a8e23bc322e5bb1a01b347acfbde11
Andresbd <andres.bd@outlook.com>
2018-10-08 12:35:36 -0500

    First submit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	public static bool passed, save;
+	private GameManager GM;
+	
+	void Start () {
+        
+		//Check if a Game Manager exists
+		if (GM == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			GM = this;
+		}else if (GM != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	private void Update()
+	{
+		if (passed)
+		{
+		}
+
+		if (save)
+		{
+		}
+	}
+}
---
+++ Assets/Scripts/InterfaceManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InterfaceManager : MonoBehaviour
+{
+
+	public void LoadGames()
+	{
+		SceneManager.LoadScene("Games");
+	}
+
+	public void LoadScore()
+	{
+		SceneManager.LoadScene("Score");
+	}
+
+	public void LoadOptions()
+	{
+		SceneManager.LoadScene("Options");
+	}
+
+	public void LoadMenu()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+}
